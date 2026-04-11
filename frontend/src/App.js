a7e2e84dead8d9c4ea04a5417ab15a78358ac733
Joel <joelstxn@gmail.com>
2026-04-11 18:05:09 +0100

    added navbar and basic google maps implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Map from './components/map/index';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <NavBar />
             <Routes>
                 <Route path="/register" element={<Register />} />
+                <Route path="/map" element={<Map />} />
             </Routes>
         </Router>
     );
